47bcc8873c5e41b56cead67c2ef64269ac7c17de
Geverson Araujo <gever.fernandes@gmail.com>
2022-10-28 21:01:42 -0300

    Atividade 01 - Typescript

---
+++ index.ts
@@ -0,0 +1,6 @@
+class Validator{
+    public data: number | string | boolean | void | null | undefined
+    constructor(data: number | string | boolean | void | null | undefined){
+        this.data = data
+    }
+}
