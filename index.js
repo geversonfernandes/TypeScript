c4cccc7d211ae7c4cd0881714bfff679b61bac2b
Geverson Araujo <gever.fernandes@gmail.com>
2022-11-04 05:55:06 -0300

    Atividade 03 - shadow e custom elements.

--- index.js
+++ index.js
@@ -4,3 +4,48 @@ class Validator {
         this.data = data;
     }
 }
+class StringValidator extends Validator {
+    constructor(data) {
+        if (typeof data === "string") {
+            super(data);
+            console.log(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+class NumberValidator extends Validator {
+    constructor(data) {
+        if (typeof data === "number") {
+            super(data);
+            console.log(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+class BooleanValidator extends Validator {
+    constructor(data) {
+        if (typeof data === "boolean") {
+            super(data);
+            console.log(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+const test1 = new StringValidator("Testando String.");
+const test2 = new NumberValidator(10);
+const test3 = new BooleanValidator("Forçando Erro.");
+class EmailInput extends HTMLElement {
+    constructor() {
+        super();
+        this.input = document.createElement('input');
+        this.root = document.createElement('div').appendChild(this.input);
+        this.shadow = this.attachShadow({ mode: 'open' }).appendChild(this.root);
+    }
+}
+customElements.define('email-input', EmailInput);
--- index.ts
+++ index.ts
@@ -4,3 +4,52 @@ class Validator{
         this.data = data
     }
 }
+
+class StringValidator extends Validator {
+    constructor(data: any) {
+        if (typeof data === "string") {
+            super(data);
+            console.log(data);
+        } else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+
+class NumberValidator extends Validator {
+    constructor(data: any) {
+        if (typeof data === "number") {
+            super(data);
+            console.log(data);
+        } else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+
+class BooleanValidator extends Validator {
+    constructor(data: any) {
+        if (typeof data === "boolean") {
+            super(data);
+            console.log(data);
+        } else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+
+const test1 = new StringValidator("Testando String.");
+const test2 = new NumberValidator(10);
+const test3 = new BooleanValidator("Forçando Erro.");
+
+class EmailInput extends HTMLElement{
+    constructor(){
+        super()
+    }
+
+    input = document.createElement('input')
+    root = document.createElement('div').appendChild(this.input)
+    shadow = this.attachShadow({ mode: 'open' }).appendChild(this.root)
+}
+
+customElements.define('email-input', EmailInput)
